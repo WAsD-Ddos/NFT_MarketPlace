5119a88c5215220ad4d32e6b280b56ac2f30687b
WAsD-Ddos <mitja3375@gmail.com>
2025-09-30 23:46:23 +0300

    project redesigned, add filter cards

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+//input section
 const searchForm = document.querySelector('.search');
 const searchInput = document.querySelector('.search__input');
 const searchButton = document.querySelector('.search__button');
@@ -9,15 +10,15 @@ let typingInterval;
 searchInput.addEventListener('focus', () => {
 
   clearInterval(typingInterval);
-  
-  
+
+
   searchInput.setAttribute('data-placeholder', originalPlaceholder);
   searchInput.removeAttribute('placeholder');
   searchInput.value = '';
 });
 
 searchInput.addEventListener('blur', () => {
-   clearInterval(typingInterval);
+  clearInterval(typingInterval);
   if (!searchInput.value && searchInput.getAttribute('data-placeholder')) {
     startTypingAnimation();
     searchInput.removeAttribute('data-placeholder');
@@ -30,9 +31,9 @@ function startTypingAnimation() {
   const fullText = searchInput.getAttribute('data-placeholder') || '';
   let currentText = '';
   let index = 0;
-  
+
   clearInterval(typingInterval);
-  
+
   typingInterval = setInterval(() => {
     if (index < fullText.length) {
       currentText += fullText[index];
@@ -41,7 +42,7 @@ function startTypingAnimation() {
     } else {
       clearInterval(typingInterval);
     }
-  }, 100); 
+  }, 100);
 }
 
 searchInput.addEventListener('input', () => {
@@ -51,7 +52,7 @@ searchInput.addEventListener('input', () => {
   }
 });
 
-// Sidebar burger (logo) toggle for <=1024px
+// Sidebar burger section
 
 
 const sidebar = document.querySelector('.sidebar');
@@ -71,9 +72,9 @@ function closeSidebar() {
 }
 
 function handleClickOutside(event) {
-  if (!sidebar.contains(event.target) && 
-      event.target !== headerBurgerBtn && 
-      event.target !== sidebarBtn) {
+  if (!sidebar.contains(event.target) &&
+    event.target !== headerBurgerBtn &&
+    event.target !== sidebarBtn) {
     closeSidebar();
   }
 }
@@ -81,14 +82,68 @@ function handleClickOutside(event) {
 
 sidebarBtn.addEventListener('click', closeSidebar);
 
-  headerBurgerBtn.addEventListener('click', (event) => {
-    event.stopPropagation();
-    if (sidebar.classList.contains('sidebar--close')) {
-      openSidebar();
-    } else {
-      closeSidebar();
+headerBurgerBtn.addEventListener('click', (event) => {
+  event.stopPropagation();
+  if (sidebar.classList.contains('sidebar--close')) {
+    openSidebar();
+  } else {
+    closeSidebar();
+  }
+});
+
+
+//filtered section
+
+const filterdButtons = document.querySelector(".goods__button-group");
+const goodsCards = document.querySelectorAll('.goods__card');
+let hiddenedCards = [];
+
+function replaceButtonWithColor(target) {
+  let elem = filterdButtons.querySelector(".button__choosed");
+  elem.classList.remove('button__choosed');
+  target.classList.add('button__choosed')
+}
+
+
+function addAllCards() {
+    hiddenedCards.forEach((card) => {
+      card.classList.remove('card__hidenned');
+    })
+  hiddenedCards = [];
+}
+
+
+
+function filterCards(type) {
+  addAllCards();
+  goodsCards.forEach((card) => {
+    const typeOf = card.getAttribute('type_of');
+    if (typeOf !== type) {
+      card.classList.add('card__hidenned');
+      hiddenedCards.push(card);
     }
-  });
+  })
+}
+
+filterdButtons.addEventListener('click', function (e) {
+  if (e.target.classList.contains('goods__button')) {
+    replaceButtonWithColor(e.target);
+    let art = 'art';
+    let pixel = 'pixel';
+    if (e.target.value === art) {
+      filterCards(art)
+    }
+    else if (e.target.value === pixel) {
+      filterCards(pixel)
+    }
+    else {
+      addAllCards();
+    }
+  }
+})
+
+
+
 
 
 
